cf558afe8a326e9c074a2eb96a0d65c36fe29bf8
Carlos Aponte <carlos@theflou.com>
2025-06-13 16:06:18 -0400

    feat: add button navigator nav

--- front/src/app/pages/dashboard/components/sidebar/sidebar.component.ts
+++ front/src/app/pages/dashboard/components/sidebar/sidebar.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../services/auth.service';
+
+interface NavItem {
+  id: string;
+  label: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +14,20 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  activeItem: string = 'weather';
+  navItems: NavItem[] = [
+    { id: "weather", label: "Weather", icon: "🌤️" },
+    { id: "users", label: "Usuarios", icon: "⬆️" },
+    { id: "logout", label: "Salir", icon: "🚪" },
+  ]
 
+  constructor(private authService: AuthService) {}
+
+  handleNavClick(navItem: NavItem): void {
+    if (navItem.id === 'logout') {
+      this.authService.logout();
+    } else {
+      this.activeItem = navItem.id;
+    }
+  }
 }
